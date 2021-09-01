c1175970a8bb4bb0abf07a37e2c24f0cfae68177
timothyjin <tjin316@gmail.com>
2021-09-01 19:20:21 -0400

    Remove old bumblebee screen layout scripts
    We haven't use bumblebee in so long so we can probably just delete
    these.

--- .screenlayout/e-V2L-V1A.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output eDP1 --mode 1920x1080 --pos 0x180 --rotate normal --output VIRTUAL1 --mode VIRTUAL1.448-1920x1080 --pos 4480x180 --rotate normal --output VIRTUAL2 --primary --mode VIRTUAL2.456-2560x1440 --pos 1920x0 --rotate normal --output VIRTUAL3 --off --output VIRTUAL4 --off
